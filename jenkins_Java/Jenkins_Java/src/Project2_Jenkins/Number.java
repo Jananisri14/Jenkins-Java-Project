fdc82265be0e6bb0e67fe6e1aefd03588a2aa345
Jananisri14 <jananisathishkumar380@gmail.com>
2026-03-09 16:22:15 +0530

    first jenkin commit

---
+++ jenkins_Java/Jenkins_Java/src/Project2_Jenkins/Number.java
@@ -0,0 +1,15 @@
+package Project2_Jenkins;
+
+public class Number {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("Enter the number");
+		int n=10;
+		System.out.println(n);
+	}
+}
+
+
+	
---
+++ jenkins_Java/src/Project2_Jenkins/Number.java
@@ -0,0 +1,14 @@
+package Project2_Jenkins;
+
+public class Number {
+
+	public static void main(String[] args) {
+		
+
+			
+				
+				System.out.println("Enter the number");
+				int n=10;
+				System.out.println(n);
+			}
+		}
